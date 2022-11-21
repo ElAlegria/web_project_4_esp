485717424940dbbc0d1b91dad7e77fbcebd039c6
Cristian Alegria <abraham.32v@gmail.com>
2022-11-20 23:21:07 -0600

    proyecto terminado a la espera de revicion

---
+++ scripts/popup.js
@@ -0,0 +1,34 @@
+const popup = document.querySelector(".popup");
+const popupContainer = popup.querySelector(".popup__container");
+const ButtonClose = popup.querySelector(".popup__close-icon");
+
+const ButtonEdit = document.querySelector(".profile__edit-button");
+
+function toggleForm() {
+
+    popup.classList.toggle("popup__show");
+    popupContainer.classList.toggle("popup__scale");
+
+}
+
+ButtonEdit.addEventListener("click",toggleForm);
+ButtonClose.addEventListener("click",toggleForm);
+
+
+let inputname = popupContainer.querySelector(".popup__name");
+let inputjob = popupContainer.querySelector(".popup__job");
+
+const ProfileName = document.querySelector(".profile__name");
+const ProfielJob = document.querySelector(".profile__job");
+
+function  handleProfileFormSubmit(event) {
+
+    event.preventDefault();
+    
+    ProfileName.textContent = inputname.value;
+    ProfielJob.textContent = inputjob.value;
+
+     toggleForm();
+}
+
+popupContainer.addEventListener("submit",handleProfileFormSubmit);
