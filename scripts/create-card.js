352fc165f4e6fc7fe057889c76f48df14246b829
Cristian Alegria <abraham.32v@gmail.com>
2022-12-04 23:14:15 -0600

    implementacion de cards dinamicas y efectos(concluidas)

--- scripts/create-card.js
+++ scripts/create-card.js
@@ -5,9 +5,6 @@ const buttonCloseCard = card.querySelector(".create-card__close-icon");
 const buttonAdd = document.querySelector(".profile__add-button");
 const inputImageCard = cardContainer.querySelector(".create-card__image");
 const InputNameCard = cardContainer.querySelector(".create-card__name");
-const cardTemplate = document
-  .querySelector(".cards-template")
-  .content.querySelector(".cards__card");
 const usuarioCards = document.querySelector(".cards");
 
 //funciones para añadir tarjetas
@@ -15,68 +12,71 @@ const usuarioCards = document.querySelector(".cards");
 
 //estructura de array para almacenar las info de las cards
 
-const initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  },
-];
+const initialCards = [];
 
-function CreateCards() {
-  const cardName = document.querySelector(".cards__title");
-  const cardImage = document.querySelector(".cards__image");
+function CreateCards(titleValue, linkValue) {
+  const cardTemplate = document
+    .querySelector(".cards-template")
+    .content.querySelector(".cards__card");
+
   const node = cardTemplate.cloneNode(true);
 
-  //  cardImage.textContent = initialCards.name;
-
-  cardsalmacen.push(node);
+  node.querySelector(".cards__title").textContent = titleValue;
+  node.querySelector(".cards__image").src = linkValue;
   usuarioCards.append(node);
+  initialCards.push(node);
+
+  node.querySelector(".cards__heart").addEventListener("click", function (evt) {
+    evt.target.classList.toggle("cards__heart_active");
+  });
+
+  const removebutton = node
+    .querySelector(".cards__remove")
+    .addEventListener("click", function (evt) {
+      node.classList.add("animation__position-right");
+      setTimeout(() => {
+        node.classList.remove("animation__position-right");
+        node.remove();
+      }, 1200);
+    });
+
+  return node;
 }
 
 // abrir,cerrar y guardar formulario de cards
-buttonAdd.addEventListener("click", () => {
-  card.classList.add("animation__show");
-  cardContainer.classList.add("animation__scale");
-});
-
-buttonCloseCard.addEventListener("click", () => {
-  card.classList.remove("animation__show");
-  card.classList.remove("animation__show-");
-});
+function animationsForCardsAndForm() {
+  buttonAdd.addEventListener("click", () => {
+    card.classList.add("animation__show");
+    cardContainer.classList.add("animation__scale");
+  });
+}
 
 function toggleFormCardReverse() {
-  card.classList.remove("animation__show");
-  cardContainer.classList.remove("animation__scale");
+  card
+    .querySelector(".create-card__overlay")
+    .classList.add("animation__show-reverse");
+  cardContainer.classList.add("animation__position-right");
+
+  setTimeout(() => {
+    card
+      .querySelector(".create-card__overlay")
+      .classList.remove("animation__show-reverse");
+    card.classList.remove("animation__show");
+    cardContainer.classList.remove("animation__position-right");
+  }, 1300);
 }
-
-function handleCreateCardFormSubmitCard(event) {
-  event.preventDefault();
-
+buttonCloseCard.addEventListener("click", () => {
   toggleFormCardReverse();
-  CreateCards();
-  initialCards.push({ name: InputNameCard.value, link: inputImageCard.value });
+});
+
+cardContainer.addEventListener("submit", (event) => {
+  event.preventDefault();
+  toggleFormCardReverse();
+  animationsForCardsAndForm();
+  CreateCards(InputNameCard.value, inputImageCard.value);
+
   inputImageCard.value = "";
   InputNameCard.value = "";
-}
+});
 
-cardContainer.addEventListener("submit", handleCreateCardFormSubmitCard);
+animationsForCardsAndForm();
